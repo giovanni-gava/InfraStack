b6f88ff9517d2553c338cafe85d27c9f593da8fc
Giovanni Alves da Silva Gava <giovanni.aalvesdasilva@gmail.com>
2024-08-24 14:42:04 -0300

    adicionando terraform provider.tf

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -1,19 +1,19 @@
 variable "access_key" {
     type = string
     description = "access key id"
-    default = "ASIAQ57ORHQLXHWHER4L"
+    default = ""
 }
 
 variable "secret_key" {
     type = string
     description = "secret key"
-    default = "nBpJ+uFKAO5NW4ZsP4bNOW6oVzp/aSD5GrZOcb/b"
+    default = ""
 }
 
 variable "session_token" {
     type = string
     description = "session token"
-    default = "IQoJb3JpZ2luX2VjELn//////////wEaCXVzLXdlc3QtMiJHMEUCIQD8ctCkqTeiY85gxraywskI6nUYj6QShf2C9PJBmVUvSwIgVUAt4wzvUJOIFuy/8UiUaEmq0VdDirW73AqddDNgo68qqgIIwv//////////ARAAGgwwNjQzODc4OTAxOTkiDDny11FiEsBQ8+5g2ir+AQBTsTth3/U9tHmo9pDgKLUuah/CLLa49cW2EEGve67f6EvZQql/xgss3txQfhMVhGWcxJBmAsj1XF6cTva258MPXpSYyaHRHKxgRWZmK10HVHQPhFPfZjXgyk0Iiuc1J/3cZSLYB3bsq5pqhIYLbHqF/3o0pufL4JWTSGqKrmjXXAQjuMhOvzEQiO3pSPRY2AA8EYwjnaGdHh/iVoQFTN8FDHquomPvEvW41PIkQQ5uxzaPbNvOqIN1l9VCAOzJvURDwPyS1T3kqawDjXvQdGaq/JVDX9812pzFxqhh+x5gzOosrJL9qqJ//ai1I6RHjQU7aCY0cDF9cbCgp+gnMOaTqLYGOp0ByQ04QKaGa8hOL64WJfKmE1zPZSp0asMDRd8WmjdmPhtMMMWlBORt6ycTGa7EB19xDRJd/YZ82PVd+RAdPt8ovthLpPqOyPWmEQleCogDxEnr2tDAxeURLr4T8sXEJSZOfxIbK1cJecfOCHdpbrsGuFQWXQkYk8pGvJAahhh57zdmn9QMlU9jxlVW6wSXTWWzX6/kD9pjWfo3roX7DA=="
+    default = ""
   
 }
 
