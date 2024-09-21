78d0cda4693e102314098077713daf3c87626ebd
Giovanni Alves da Silva Gava <10429013@educa.mackenzie.br>
2024-09-21 14:38:47 -0300

    feat(tf): add numerous files

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "aws-mack-s3-tfstate-giovanni-2024"
+    bucket = "aws-mack-s3-tfstate-actions-pipeline"
     key = "terraform.tfstate"
     region = "us-east-1"
     
---
+++ infra/env/dev/terraform.tfvars
@@ -0,0 +1,2 @@
+pipe_bucket_prefix = "aws-dev-mack-s3-actions-tf-giovanni"
+pipe_region = "us-east-1"
